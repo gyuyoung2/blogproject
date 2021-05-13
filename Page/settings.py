0b7c403480913990755a06d6c6a9cd8d9308e862
gyuyoung2 <tkrhk2836@naver.com>
2021-05-13 22:01:46 +0900

    e

--- Page/settings.py
+++ Page/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    
+
 ]
 
 ROOT_URLCONF = 'Page.urls'
@@ -77,13 +78,25 @@ WSGI_APPLICATION = 'Page.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'daek75r7hpalvg', #DATABASE
+        'USER' : 'qerbugeulcbbes',
+        'PASSWORD' : 'de55e6af377233ba641ca5ad99069a3ef4f4ca915ba1c171fd8eff51dafc72e7',
+        'HOST':'ec2-23-23-128-222.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
